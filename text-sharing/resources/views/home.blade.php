ae4df4f9cc9d0aa1dbf89f02091cf6ac993684f0
Mohak157 <girimohak73@gmail.com>
2026-09-05 16:44:48 +0545

    chore:cleaned up all the bugs and inconsistencies

--- text-sharing/resources/views/home.blade.php
+++ text-sharing/resources/views/home.blade.php
@@ -23,7 +23,7 @@
 
                     <div class="mt-4 flex items-center justify-end">
                         <button type="submit" class="btn btn-primary btn-sm">
-                            Chirp
+                            Send
                         </button>
                     </div>
 
