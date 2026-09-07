0aa7271c1259dc62bd459789533d805181f7a967
Mohak157 <girimohak73@gmail.com>
2026-09-07 20:17:08 +0545

    feat:added full auth login logout signup

--- text-sharing/app/Http/Controllers/Auth/Logout.php
+++ text-sharing/app/Http/Controllers/Auth/Logout.php
@@ -13,8 +13,12 @@ class Logout extends Controller
      */
     public function __invoke(Request $request)
     {
-        $user = $request ->user();
-        Auth::logout($user);
+        
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
 
         return redirect('/')->with('success', 'logged out');
     }
--- text-sharing/routes/web.php
+++ text-sharing/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\MsgController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\Auth\Logout;
+use App\Http\Controllers\Auth\Login;
 
 Route::get('/',[MsgController::class,'index']);
 
@@ -18,3 +19,5 @@ Route::middleware('auth')->group(function(){
 Route::view('/register','auth.register') ->middleware('guest')->name('register');
 Route::post('/register',Register::class)->middleware('guest');
 Route::post('/logout',Logout::class)->middleware('auth');
+Route::view('/login','auth.login')->middleware('guest');
+Route::post('/login',Login::class)->middleware('guest');
